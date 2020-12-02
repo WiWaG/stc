d547e45bb2a9c65318b114dbe5dbdcae0cef7580
WiWaG <willem@wiwaweb.nl>
2020-12-02 13:36:34 +0100

    edit post.js

--- stc/models/post.js
+++ stc/models/post.js
@@ -1,5 +1,5 @@
-//Wouter
 const mongoose = require("mongoose");
+const Comment = require("./comment");
 const Schema = mongoose.Schema;
 
 const PostSchema = new Schema({
@@ -7,46 +7,48 @@ const PostSchema = new Schema({
         type: String,
         required: true
     },
-
     body: {
         type: String,
         required: true
     },
-
     date: {
         type: {
             type: Date,
             default: Date.now
         }
     },
-
     author: {
         type: Schema.Types.ObjectId,
         ref: "User"
     },
-
     comments: [{
         type: Schema.Types.ObjectId,
         ref: "Comment"
     }],
-
     categoryType: {
         type: String,
         required: true,
         enum: ["aanbod", "vraag"],
         lowercase: true
     },
-
     categoryRequestType: {
         type: String,
         lowercase: true
     },
-
     region: {
         type: String,
         lowercase: true
     }
+});
 
+PostSchema.post("findOneAndDelete", async function (doc) {
+    if (doc) {
+        await Comment.deleteMany({
+            _id: {
+                $in: doc.comments
+            }
+        });
+    }
 });
 
 module.exports = mongoose.model("Post", PostSchema);
